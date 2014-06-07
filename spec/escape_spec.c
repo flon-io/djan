7f262c2760b228a0040914905957a553f467e342
John Mettraux <jmettraux@gmail.com>
2014-06-07 09:55:58 +0900

    scaffold spec/escape_spec.c

---
+++ spec/escape_spec.c
@@ -0,0 +1,36 @@
+
+//
+// specifying djan
+//
+// Sat Jun  7 09:41:22 JST 2014
+//
+
+#include "djan.h"
+
+
+context "escapes"
+{
+  describe "dja_escape()"
+  {
+    it "escapes"
+  }
+
+  describe "dja_n_escape()"
+  {
+    it "escapes"
+  }
+
+  describe "dja_unescape()"
+  {
+    it "unescapes tabs"
+    {
+      ensure(dja_unescape("a \\ttab") ===f "a\ttab");
+    }
+  }
+
+  describe "dja_n_unescape()"
+  {
+    it "unescapes"
+  }
+}
+
