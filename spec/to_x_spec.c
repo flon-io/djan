7831aef13574bad4e47c371d3fb7f94e8076dfb9
John Mettraux <jmettraux@gmail.com>
2014-10-02 20:57:33 +0900

    add spec for dja_to_json()

---
+++ spec/to_x_spec.c
@@ -0,0 +1,33 @@
+
+//
+// specifying djan
+//
+// Thu Oct  2 20:54:34 JST 2014
+//
+
+#include "djan.h"
+
+
+context "fdja_value"
+{
+  before each
+  {
+    fdja_value *v = NULL;
+  }
+  after each
+  {
+    if (v != NULL) fdja_value_free(v);
+  }
+
+  describe "fdja_to_json()"
+  {
+    it "turns a fdja_value to a json string"
+    {
+      v = fdja_v("{ type: car, make: mitsubishi, id: 2 }");
+
+      expect(fdja_to_json(v) ===f ""
+        "{\"type\":\"car\",\"make\":\"mitsubishi\",\"id\":2}");
+    }
+  }
+}
+
